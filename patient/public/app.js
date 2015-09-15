27144556259d58b111ba97290d21e429478ea5f6
Kathia Barahona <katbara03@gmail.com>
2015-09-15 10:50:55 -0600

    redirects done for patients

--- patient/public/app.js
+++ patient/public/app.js
@@ -1,50 +1,80 @@
 'use strict';
 angular.module('patientApp', ['ui.router'])
-    .config([
+
+
+.config([
         '$stateProvider',
         '$urlRouterProvider',
         function($stateProvider, $urlRouterProvider) {
-
             $stateProvider
                 .state('home', {
                     url: '/home/:patientID',
                     templateUrl: 'views/appointment.view.html',
-                    controller: function($scope, $stateParams) {
-                        $scope.patientid = $stateParams.patientID
-                        if ($scope.patientid == '') {
-                            //  window.location.href = "#/login"
-                        }
-
-                    }
+                    controller: 'appointmentCtrl'
                 })
                 .state('history', {
                     url: '/history/:patientID',
                     templateUrl: 'views/history.view.html',
-                    controller: function($scope, $stateParams) {
-                        $scope.patientid = $stateParams.patientID
-                        if ($scope.patientid == '') {}
-                        //  window.location.href = "#/login"
-                    }
+                    controller: 'historyCtrl'
                 })
                 .state('profile', {
                     url: '/profile/:patientID',
                     templateUrl: 'views/profile.view.html',
-                    controller: function($scope, $stateParams) {
-                        $scope.patientid = $stateParams.patientID
-                        if ($scope.patientid == '') {}
-                        // window.location.href = "#/login"
-                    }
+                    controller: 'profileCtrl'
                 })
                 .state('login', {
                     url: '/login',
                     templateUrl: 'views/login.view.html',
-                    controller: function($scope, $stateParams) {
-                        // $('.navbar-right').hide()
-                        
-
-                    }
+                    controller: 'loginCtrl'
                 })
 
             $urlRouterProvider.otherwise('login');
         }
     ])
+    .directive('navbar', function() {
+            return {
+                restrict: 'E',
+                templateUrl: 'views/navbar.view.html',
+                controller: function($scope, $stateParams) {
+
+                    $scope.redirect = function(value) {
+                        if (value == 0) {
+                            var url = "#/home/" + $stateParams.patientID
+                            window.location.href = url;
+                        }
+                        if (value == 1) {
+                            var url = "#/history/" + $stateParams.patientID
+                            window.location.href = url;
+                        }
+                        if (value == 2) {
+                            var url = "#/profile/" + $stateParams.patientID
+                            window.location.href = url;
+                        }
+                       
+                    }
+                }
+            }
+        }
+
+    )
+    .controller('appointmentCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
+        $scope.patientid = $stateParams.patientID
+        if ($scope.patientid == '' || $scope.patientid == 'undefined' ) {
+            window.location.href = "#/login"
+        }
+    }])
+    .controller('historyCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
+        $scope.patientid = $stateParams.patientID
+        if ($scope.patientid == '' || $scope.patientid == 'undefined') {
+            window.location.href = "#/login"
+        }
+    }])
+    .controller('profileCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
+        $scope.patientid = $stateParams.patientID
+        if ($scope.patientid == '' || $scope.patientid == 'undefined') {
+            window.location.href = "#/login"
+        }
+    }])
+    .controller('loginCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
+
+    }])
